3f5c25ef7bd03debe204ec964598d4fdf5298e5c
maximopiu18 <maximo_piu18@hotmail.com>
2019-07-06 17:25:24 -0500

    Anexo de ImageView

---
+++ app/src/main/java/com/example/holamundo/Imagenes/Activity_Imagenes.java
@@ -0,0 +1,15 @@
+package com.example.holamundo.Imagenes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.holamundo.R;
+
+public class Activity_Imagenes extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagenes);
+
+    }
+}
